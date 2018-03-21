efd13a52a9900593741ccf62ada8dcba7534f21d
Bruce Conrad <bruce_conrad@byu.edu>
2018-03-21 17:14:00 -0600

    typo and need txt

--- server/wrmyers68.cgi.bash
+++ server/wrmyers68.cgi.bash
@@ -5,8 +5,8 @@ TAG=`echo "$QUERY_STRING" | grep -o "id=[0-9]*" | cut -d = -f 2`
 ECI="3zr2xyNrWDwZf534UG1UHW"
 RID="com.bruceatbyu.graduands_collection"
 G_RID="com.wrmyers68.grad"
-G_ECI=`curl localhost:3002/sky/cloud/$ECI/$RID/Tx?grad=g$TAG`
-if [ -n "G_ECI" ]
+G_ECI=`curl localhost:3002/sky/cloud/$ECI/$RID/Tx.txt?grad=g$TAG`
+if [ -n "$G_ECI" ]
 then
   curl localhost:3002/sky/cloud/$G_ECI/$G_RID/grad_page.html?grad=g$TAG
 else
