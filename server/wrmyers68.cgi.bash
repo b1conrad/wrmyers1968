c23d92d9a39acbb1e38857913f8543333a0d42d7
Bruce Conrad <bruce_conrad@byu.edu>
2018-03-21 16:37:54 -0600

    use grad app when available

--- server/wrmyers68.cgi.bash
+++ server/wrmyers68.cgi.bash
@@ -4,4 +4,11 @@ echo
 TAG=`echo "$QUERY_STRING" | grep -o "id=[0-9]*" | cut -d = -f 2`
 ECI="3zr2xyNrWDwZf534UG1UHW"
 RID="com.bruceatbyu.graduands_collection"
-curl localhost:3002/sky/cloud/$ECI/$RID/grad_page.html?grad=g$TAG
+G_RID="com.wrmyers68.grad"
+G_ECI=`curl localhost:3002/sky/cloud/$ECI/$RID/Tx?grad=g$TAG`
+if [ -n "G_ECI" ]
+then
+  curl localhost:3002/sky/cloud/$G_ECI/$G_RID/grad_page.html?grad=g$TAG
+else
+  curl localhost:3002/sky/cloud/$ECI/$RID/grad_page.html?grad=g$TAG
+fi
